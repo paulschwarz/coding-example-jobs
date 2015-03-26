5263df5cf567e500a68d2d24b3f9acec37a48a90
Paul Schwarz <paulsschwarz@gmail.com>
2015-03-26 14:57:18 +0000

    Adds Job model.

---
+++ src/Jobs/Job.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jobs;
+
+class Job
+{
+    private $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
